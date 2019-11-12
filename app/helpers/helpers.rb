3f330205211f887a74b789a5b8d9a5acc2d9fd34
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-12 22:23:21 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,15 +1,10 @@
 class Helpers
 
   def self.current_user(session)
-    current_user = User.find_by(session[:user_id])
-    current_user
+    User.find_by(id: session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    if session[:user_id]
-      true
-    else
-      false
-    end
+    !!self.current_user(session)
   end
 end
