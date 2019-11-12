e4f7f470a7785d0c3140b9b0264b36e1863b4459
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-12 21:31:42 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,12 @@
 class Helpers
 
-  def self.current_user(hash)
-    current_user = User.find_by(hash[:user_id])
+  def self.current_user(session)
+    current_user = User.find_by(session[:user_id])
     current_user
   end
 
-  def is_logged_in?(hash)
-    if hash[:user_id]
+  def is_logged_in?(session)
+    if session[:user_id]
       true
     else
       false
