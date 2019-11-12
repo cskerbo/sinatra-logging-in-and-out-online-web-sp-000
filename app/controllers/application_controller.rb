3e93fbcb9da1512e6e826469ad6a730bf860d2dc
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-12 22:10:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = Helpers.current_user(session: session)
-    @logged_in = @user.is_logged_in?
+    @logged_in = @user.is_logged_in?(session: session)
     erb :account
   end
 
