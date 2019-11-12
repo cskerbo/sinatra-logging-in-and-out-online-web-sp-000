5f3a2f737f5567c9ac700f293a104b18a284d6e6
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-12 23:02:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if user
-      session[:user_id] = user.id
+    if @user
+      session[:user_id] = @user.id
       redirect '/account'
     end
     erb :error
